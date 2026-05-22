18af5ed74a92fdbb9e9f6bb9f23114b52e9aee3d
Alliah Alexis Cinco <alliah.alexiscinco@gmail.com>
2026-05-22 11:27:01 +0800

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 // === Security & Middleware ===
 app.use(helmet());
 app.use(cors({ origin: '*', optionsSuccessStatus: 200 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
